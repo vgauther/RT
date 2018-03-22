8035c17b8d1e21ed77bbf4bc9c07cc8ee1b76c0d
vgauther <vgauther@student.42.fr>
2018-03-22 12:20:25 +0100

    GIT ADD * via Makefile

--- save/lib/SDL/build/.libs/SDL_audiodev.d
+++
@@ -1,9 +0,0 @@
-build/SDL_audiodev.lo: \
-  /Users/vgauther/vgauther/Rt/save/lib/SDL2-2.0.5/src/audio/SDL_audiodev.c \
-  /Users/vgauther/vgauther/Rt/save/lib/SDL2-2.0.5/src/audio/../SDL_internal.h \
-  /Users/vgauther/vgauther/Rt/save/lib/SDL2-2.0.5/src/audio/../dynapi/SDL_dynapi.h \
-  /Users/vgauther/vgauther/Rt/save/lib/SDL2-2.0.5/src/audio/../dynapi/SDL_dynapi_overrides.h \
-  include/SDL_config.h \
-  /Users/vgauther/vgauther/Rt/save/lib/SDL2-2.0.5/include/SDL_platform.h \
-  /Users/vgauther/vgauther/Rt/save/lib/SDL2-2.0.5/include/begin_code.h \
-  /Users/vgauther/vgauther/Rt/save/lib/SDL2-2.0.5/include/close_code.h
