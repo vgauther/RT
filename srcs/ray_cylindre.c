b9f7f181c6cd7f17c13b0da810af4cf90fcec181
Florian DE SOUZA <fde-souz@student.42.fr>
2018-03-27 11:40:17 +0200

    GIT ADD * via Makefile

--- srcs/ray_cylindre.c
+++ srcs/ray_cylindre.c
@@ -6,7 +6,7 @@
 /*   By: vgauther <vgauther@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2018/03/16 12:53:51 by vgauther          #+#    #+#             */
-/*   Updated: 2018/03/23 16:58:13 by fde-souz         ###   ########.fr       */
+/*   Updated: 2018/03/26 16:44:11 by fde-souz         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -55,21 +55,58 @@ t_vec			ft_rotate(t_vec p, double rot_x, double rot_y, double rot_z)
 	return (v);
 }
 
+// t_inter		ray_cylindre(t_env *e, int i, int j, t_cam ca, int nbr)
+// {
+// 	t_inter		pt;
+// 	t_polynome	p;
+// 	t_vec		v;
+//
+// 	v = vector_init((i - SIZE_X / 2), (j - SIZE_Y / 2),
+// 	(SIZE_X / 2) / tan(30 * RAD));
+// 	p.a = v.x * v.x + v.z * v.z;
+// 	p.b = 2 * (v.x * (ca.xr - e->obj[nbr].pos.x) +
+// 	v.z * (ca.zr - e->obj[nbr].pos.z));
+// 	p.c = (ca.xr - e->obj[nbr].pos.x) * (ca.xr - e->obj[nbr].pos.x) +
+// 	(ca.zr - e->obj[nbr].pos.z) * (ca.zr - e->obj[nbr].pos.z) -
+// 	e->obj[nbr].rayon * e->obj[nbr].rayon;
+// 	p.delta = powf(p.b, 2) - 4 * p.a * p.c;
+// 	if (p.delta >= 0)
+// 	{
+// 		if (p.delta > 0)
+// 		{
+// 			p.x1 = (-p.b + sqrt(p.delta)) / (2 * p.a);
+// 			p.x2 = (-p.b - sqrt(p.delta)) / (2 * p.a);
+// 			pt.dist = p.x1 < p.x2 ? p.x1 : p.x2;
+// 			if (pt.dist < 0)
+// 				pt.dist = p.x1 < 0 ? p.x2 : p.x1;
+// 		}
+// 		else
+// 			p.x1 = -p.b / (2 * p.a);
+// 		if (!(p.x1 < 0 && p.x2 < 0))
+// 			intersection_point(&pt, ca, v);
+// 	}
+// 	else
+// 		pt.dist = -1;
+// 	return (pt);
+// }
+
 t_inter		ray_cylindre(t_env *e, int i, int j, t_cam ca, int nbr)
 {
 	t_inter		pt;
 	t_polynome	p;
 	t_vec		v;
+	t_vec		unit;
+	t_vec		x;
+	t_point		o;
 
-	v = vector_init((i - SIZE_X / 2), (j - SIZE_Y / 2),
-	(SIZE_X / 2) / tan(30 * RAD));
-	p.a = v.x * v.x + v.z * v.z;
-	p.b = 2 * (v.x * (ca.xr - e->obj[nbr].pos.x) +
-	v.z * (ca.zr - e->obj[nbr].pos.z));
-	p.c = (ca.xr - e->obj[nbr].pos.x) * (ca.xr - e->obj[nbr].pos.x) +
-	(ca.zr - e->obj[nbr].pos.z) * (ca.zr - e->obj[nbr].pos.z) -
-	e->obj[nbr].rayon * e->obj[nbr].rayon;
-	p.delta = powf(p.b, 2) - 4 * p.a * p.c;
+	v = vector_init((i - SIZE_X / 2), (j - SIZE_Y / 2), (SIZE_X / 2) / tan(30 * RAD));
+	o = init_point(e->obj[nbr].pos.x, e->obj[nbr].pos.y, e->obj[nbr].pos.z);
+	x = vector_init(ca.xr - o.x, ca.yr - o.y, ca.zr - o.z);
+	unit = vector_init(1, -1, 0);
+	p.a = (dot(v, v) - dot(v, unit) * dot(v, unit));
+	p.b = 2.0 * (dot(v, x) - (dot(v, unit) * dot(x, unit)));
+	p.c = (dot(x, x) - dot(x, unit) * dot(x, unit)) - (e->obj[nbr].rayon * e->obj[nbr].rayon);
+	p.delta = pow(p.b, 2) - 4 * p.a * p.c;
 	if (p.delta >= 0)
 	{
 		if (p.delta > 0)
@@ -83,7 +120,11 @@ t_inter		ray_cylindre(t_env *e, int i, int j, t_cam ca, int nbr)
 		else
 			p.x1 = -p.b / (2 * p.a);
 		if (!(p.x1 < 0 && p.x2 < 0))
-			intersection_point(&pt, ca, v);
+		{
+			pt.x = ca.xr + v.x * pt.dist;
+			pt.y = ca.yr + v.y * pt.dist;
+			pt.z = ca.zr + v.z * pt.dist;
+		}
 	}
 	else
 		pt.dist = -1;
