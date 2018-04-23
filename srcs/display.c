03676efac1aacef062c48a4d26cd733e4a605db6
vgauther <vgauther@student.42.fr>
2018-04-23 15:36:29 +0200

    norme

--- srcs/display.c
+++ srcs/display.c
@@ -6,22 +6,21 @@
 /*   By: vgauther <vgauther@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2018/04/23 14:56:52 by vgauther          #+#    #+#             */
-/*   Updated: 2018/04/23 14:58:15 by vgauther         ###   ########.fr       */
+/*   Updated: 2018/04/23 15:36:11 by vgauther         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "../includes/rt.h"
 
-void	add_obj_selection_display(t_sdl *s, t_env *e)
+void	add_obj_selection_display(t_sdl *s)
 {
-	(void)e;
 	SDL_RenderCopy(s->renderer, s->tex[12], NULL, &s->hud1.r_add_obj[0]);
 	SDL_RenderCopy(s->renderer, s->tex[13], NULL, &s->hud1.r_add_obj[1]);
 	SDL_RenderCopy(s->renderer, s->tex[14], NULL, &s->hud1.r_add_obj[2]);
 	SDL_RenderCopy(s->renderer, s->tex[15], NULL, &s->hud1.r_add_obj[3]);
 }
 
-void 	add_obj_print_text(t_sdl *s, t_env *e, int nb)
+void	add_obj_print_text(t_sdl *s, t_env *e, int nb)
 {
 	print_text(ft_itoa(e->obj[nb].pos.x), s->font.color[1], s,
 	&s->hud1.add_obj_data[0]);
@@ -37,22 +36,94 @@ void 	add_obj_print_text(t_sdl *s, t_env *e, int nb)
 	&s->hud1.add_obj_data[5]);
 }
 
+void	display_add_obj_menu_bloc(t_sdl *s)
+{
+	int i;
+
+	i = 0;
+	while (i < 6)
+	{
+		if ((SDL_RenderCopy(s->renderer, s->tex[0], NULL,
+			&s->hud1.text_box[i])) < 0)
+			ft_sdl_error("Texture error : ", SDL_GetError());
+		i++;
+	}
+}
+
+void	display_add_obj_menu_data(t_sdl *s)
+{
+	int i;
+
+	i = 0;
+	while (i < 6)
+	{
+		if ((SDL_RenderCopy(s->renderer, s->hud1.add_obj_data[i].tex, NULL,
+			&s->hud1.add_obj_data[i].rect)) < 0)
+			ft_sdl_error("Texture error : ", SDL_GetError());
+		i++;
+	}
+}
+
 void	add_obj_menu(t_sdl *s, t_env *e, int nb)
 {
 	add_obj_print_text(s, e, nb);
-	SDL_RenderCopy(s->renderer, s->tex[0], NULL, &s->hud1.text_box[0]);
-	SDL_RenderCopy(s->renderer, s->tex[0], NULL, &s->hud1.text_box[1]);
-	SDL_RenderCopy(s->renderer, s->tex[0], NULL, &s->hud1.text_box[2]);
-	SDL_RenderCopy(s->renderer, s->tex[0], NULL, &s->hud1.text_box[3]);
-	SDL_RenderCopy(s->renderer, s->tex[0], NULL, &s->hud1.text_box[4]);
-	SDL_RenderCopy(s->renderer, s->tex[0], NULL, &s->hud1.text_box[5]);
-	SDL_RenderCopy(s->renderer, s->hud1.add_obj_data[0].tex, NULL, &s->hud1.add_obj_data[0].rect);
-	SDL_RenderCopy(s->renderer, s->hud1.add_obj_data[1].tex, NULL, &s->hud1.add_obj_data[1].rect);
-	SDL_RenderCopy(s->renderer, s->hud1.add_obj_data[2].tex, NULL, &s->hud1.add_obj_data[2].rect);
-	SDL_RenderCopy(s->renderer, s->hud1.add_obj_data[3].tex, NULL, &s->hud1.add_obj_data[3].rect);
-	SDL_RenderCopy(s->renderer, s->hud1.add_obj_data[4].tex, NULL, &s->hud1.add_obj_data[4].rect);
-	SDL_RenderCopy(s->renderer, s->hud1.add_obj_data[5].tex, NULL, &s->hud1.add_obj_data[5].rect);
+	display_add_obj_menu_bloc(s);
+	display_add_obj_menu_data(s);
 	SDL_RenderCopy(s->renderer, s->tex[s->hud1.ok.i], NULL, &s->hud1.ok.rect);
+}
+
+void	display_cam(t_sdl *s)
+{
+	int		*r;
+	int		i;
+
+	if (!(r = (int*)malloc(sizeof(int) * 7)))
+		ft_error("\nMalloc Error\n");
+	r[0] = SDL_RenderCopy(s->renderer, s->hud1.cam.title.tex, NULL,
+		&s->hud1.cam.title.rect);
+	r[1] = SDL_RenderCopy(s->renderer, s->hud1.cam.pos_x.tex, NULL,
+		&s->hud1.cam.pos_x.rect);
+	r[2] = SDL_RenderCopy(s->renderer, s->hud1.cam.pos_y.tex, NULL,
+		&s->hud1.cam.pos_y.rect);
+	r[3] = SDL_RenderCopy(s->renderer, s->hud1.cam.pos_z.tex, NULL,
+		&s->hud1.cam.pos_z.rect);
+	r[4] = SDL_RenderCopy(s->renderer, s->hud1.cam.rot_x.tex, NULL,
+		&s->hud1.cam.rot_x.rect);
+	r[5] = SDL_RenderCopy(s->renderer, s->hud1.cam.rot_y.tex, NULL,
+		&s->hud1.cam.rot_y.rect);
+	r[6] = SDL_RenderCopy(s->renderer, s->hud1.cam.rot_z.tex, NULL,
+		&s->hud1.cam.rot_z.rect);
+	i = -1;
+	while (++i < 7)
+		if (r[i] < 0)
+			ft_error("\nRender copy Error : Camera display\n");
+	free(r);
+}
+
+void	display_all_bouton(t_sdl *s)
+{
+	int i;
+
+	i = -1;
+	while (++i != 17)
+	{
+		if ((SDL_RenderCopy(s->renderer, s->tex[s->hud1.bouton[i].i], NULL,
+			&s->hud1.bouton[i].rect)) < 0)
+			ft_sdl_error("Texture error : ", SDL_GetError());
+	}
+}
+
+void	display_multi_text(t_sdl *s)
+{
+	if ((SDL_RenderCopy(s->renderer, s->hud1.multi_text[0].tex, NULL,
+		&s->hud1.multi_text[0].rect)) < 0)
+		ft_sdl_error("Texture error : ", SDL_GetError());
+	if ((SDL_RenderCopy(s->renderer, s->hud1.multi_text[1].tex, NULL,
+		&s->hud1.multi_text[1].rect)) < 0)
+		ft_sdl_error("Texture error : ", SDL_GetError());
+	if ((SDL_RenderCopy(s->renderer, s->hud1.multi_text[2].tex, NULL,
+		&s->hud1.multi_text[2].rect)) < 0)
+		ft_sdl_error("Texture error : ", SDL_GetError());
 }
 
 void	display(t_sdl *s, t_env *e)
@@ -64,6 +135,10 @@ void	display(t_sdl *s, t_env *e)
 	if (!(r = (int*)malloc(sizeof(int) * 34)))
 		ft_error("\nMalloc Error\n");
 	i = -1;
+	while (++i < 33)
+	{
+		r[i] = 0;
+	}
 	SDL_RenderClear(s->renderer);
 	recup_cam_to_print(s, e);
 	s->rendu->pixels = e->pixels;
@@ -72,17 +147,11 @@ void	display(t_sdl *s, t_env *e)
 			== NULL)
 		ft_sdl_error("Texture error : ", SDL_GetError());
 	r[0] = SDL_RenderCopy(s->renderer, s->hud1.t_back, NULL, NULL);
-	r[1] = SDL_RenderCopy(s->renderer, s->hud1.cam.title.tex, NULL, &s->hud1.cam.title.rect);
-	r[2] = SDL_RenderCopy(s->renderer, s->hud1.cam.pos_x.tex, NULL, &s->hud1.cam.pos_x.rect);
-	r[3] = SDL_RenderCopy(s->renderer, s->hud1.cam.pos_y.tex, NULL, &s->hud1.cam.pos_y.rect);
-	r[4] = SDL_RenderCopy(s->renderer, s->hud1.cam.pos_z.tex, NULL, &s->hud1.cam.pos_z.rect);
-	r[5] = SDL_RenderCopy(s->renderer, s->hud1.cam.rot_x.tex, NULL, &s->hud1.cam.rot_x.rect);
-	r[6] = SDL_RenderCopy(s->renderer, s->hud1.cam.rot_y.tex, NULL, &s->hud1.cam.rot_y.rect);
-	r[7] = SDL_RenderCopy(s->renderer, s->hud1.cam.rot_z.tex, NULL, &s->hud1.cam.rot_z.rect);
+	display_cam(s);
 	r[8] = SDL_RenderCopy(s->renderer, s->hud1.info.tex, NULL, &s->hud1.info.rect);
 	display_credits(s);
-	while (++i != 17)
-		r[i + 8] = SDL_RenderCopy(s->renderer, s->tex[s->hud1.bouton[i].i], NULL, &s->hud1.bouton[i].rect);
+	display_all_bouton(s);
+	display_multi_text(s);
 	r[24] = SDL_RenderCopy(s->renderer, s->hud1.logo.tex, NULL, &s->hud1.logo.rect);
 	r[25] = SDL_RenderCopy(s->renderer, s->texture, NULL, &rendu_rect);
 	r[26] = SDL_RenderCopy(s->renderer, s->hud1.save.tex, NULL, &s->hud1.save.rect);
@@ -91,18 +160,16 @@ void	display(t_sdl *s, t_env *e)
 	else
 		r[27] = 0;
 	r[28] = SDL_RenderCopy(s->renderer, s->tex[0], NULL, &s->hud1.multi_bouton[s->hud1.multi]);
-	r[29] = SDL_RenderCopy(s->renderer, s->hud1.multi_text[0].tex, NULL, &s->hud1.multi_text[0].rect);
-	r[30] = SDL_RenderCopy(s->renderer, s->hud1.multi_text[1].tex, NULL, &s->hud1.multi_text[1].rect);
-	r[31] = SDL_RenderCopy(s->renderer, s->hud1.multi_text[2].tex, NULL, &s->hud1.multi_text[2].rect);
 	r[33] = SDL_RenderCopy(s->renderer, s->hud1.workspace_text.tex, NULL, &s->hud1.workspace_text.rect);
 	if (s->hud1.add_obj == 1)
-		add_obj_selection_display(s, e);
+		add_obj_selection_display(s);
 	if (s->hud1.add_obj == 2)
 		add_obj_menu(s, e, e->nb - 1);
 	if (s->hud1.pipette == 1)
 		r[32] = SDL_RenderCopy(s->renderer, s->tex[16], NULL, &s->hud1.color_selector);
 	else
 		r[32] = 0;
+
 	i = -1;
 	while (++i < 34)
 		if (r[i] < 0)
