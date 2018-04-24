5bf1346b670deb570ef96adc436bf743ef97168f
Florian DE SOUZA <fde-souz@student.42.fr>
2018-04-24 17:12:20 +0200

    norm pipette

--- srcs/mouse_pipette.c
+++ srcs/mouse_pipette.c
@@ -6,7 +6,7 @@
 /*   By: vgauther <vgauther@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2018/04/19 23:21:26 by vgauther          #+#    #+#             */
-/*   Updated: 2018/04/24 16:46:12 by vgauther         ###   ########.fr       */
+/*   Updated: 2018/04/24 17:11:41 by fde-souz         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -27,32 +27,26 @@ void	mouse_pipette_activate(int x, int y, t_sdl *s, t_env *e)
 
 void	mouse_pipette_color(int x, int y, t_sdl *s, t_env *e)
 {
-	(void)x;
-	(void)y;
-	(void)s;
-	(void)e;
-	Uint32 color;
-	char *tab;
-	t_rect r1;
+	Uint32	color;
+	char	*tab;
+	int		bpp;
 
+	bpp = s->hud1.color_selector_surf->pitch / s->hud1.color_selector_surf->w;
 	x -= (WIN_X / 2) - s->hud1.color_selector_surf->w / 2;
 	y -= (WIN_Y / 2) - s->hud1.color_selector_surf->h / 2;
-	printf("%d|%d\n", s->hud1.color_selector_surf->pitch / s->hud1.color_selector_surf->w, x);
 	if (x >= 0 && x <= s->hud1.color_selector_surf->w)
 	{
 		if (y >= 0 && y <= s->hud1.color_selector_surf->h)
 		{
 			tab = (char *)s->hud1.color_selector_surf->pixels;
-			color = tab[x * (s->hud1.color_selector_surf->pitch / s->hud1.color_selector_surf->w) +
-			s->hud1.color_selector_surf->w * (s->hud1.color_selector_surf->pitch / s->hud1.color_selector_surf->w) * y];
-			color += tab[x * (s->hud1.color_selector_surf->pitch / s->hud1.color_selector_surf->w) + s->hud1.color_selector_surf->w
-			* (s->hud1.color_selector_surf->pitch / s->hud1.color_selector_surf->w) * y + 1] * 256;
-			color += tab[x * (s->hud1.color_selector_surf->pitch / s->hud1.color_selector_surf->w) + s->hud1.color_selector_surf->w
-			* (s->hud1.color_selector_surf->pitch / s->hud1.color_selector_surf->w) * y + 2]
+			color = tab[x * bpp + s->hud1.color_selector_surf->w * bpp * y];
+			color += tab[x * bpp + s->hud1.color_selector_surf->w * bpp * y + 1]
+			* 256;
+			color += tab[x * bpp + s->hud1.color_selector_surf->w * bpp * y + 2]
 			* 256 * 256;
-			r1 = init_rect(s->hud1.bouton[15].rect.x + 110, s->hud1.bouton[15].rect.y, 40, 40);
 			s->hud1.color = color;
-			print_rect(r1, e, 1, color);
+			print_rect(init_rect(s->hud1.bouton[15].rect.x + 110,
+			s->hud1.bouton[15].rect.y, 40, 40), e, 1, color);
 			actualize_background(s, e);
 			display(s, e);
 		}
