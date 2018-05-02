ca8eeb27855eb836eabcd2d02c4656da25ab9269
Florian DE SOUZA <fde-souz@student.42.fr>
2018-05-02 17:52:08 +0200

    norm

--- srcs/hud.c
+++ srcs/hud.c
@@ -6,7 +6,7 @@
 /*   By: vgauther <vgauther@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2018/03/29 13:47:14 by vgauther          #+#    #+#             */
-/*   Updated: 2018/04/29 17:17:27 by vgauther         ###   ########.fr       */
+/*   Updated: 2018/05/02 17:51:59 by fde-souz         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -45,13 +45,17 @@ void	some_traits(t_env *e)
 
 void	init_plmor(t_sdl *s)
 {
-	s->hud1.plmor[0].rect = init_sdl_rect(WIN_X - COL + 80, WIN_Y / 2 + 115, 50, 30);
-	s->hud1.plmor[1].rect = init_sdl_rect(WIN_X - COL + 140, WIN_Y / 2 + 115, 30, 30);
-	s->hud1.plmor[2].rect = init_sdl_rect(WIN_X - COL + 175, WIN_Y / 2 + 115, 30, 30);
+	s->hud1.plmor[0].rect = init_sdl_rect(WIN_X -
+		COL + 80, WIN_Y / 2 + 115, 50, 30);
+	s->hud1.plmor[1].rect = init_sdl_rect(WIN_X -
+		COL + 140, WIN_Y / 2 + 115, 30, 30);
+	s->hud1.plmor[2].rect = init_sdl_rect(WIN_X -
+		COL + 175, WIN_Y / 2 + 115, 30, 30);
 	s->hud1.plmor[0].i = 0;
 	s->hud1.plmor[1].i = 1;
 	s->hud1.plmor[2].i = 0;
-	s->hud1.add_obj_data[6].rect = init_sdl_rect(WIN_X - COL + 80, WIN_Y / 2 + 115, 50, 30);
+	s->hud1.add_obj_data[6].rect = init_sdl_rect(WIN_X -
+		COL + 80, WIN_Y / 2 + 115, 50, 30);
 }
 
 /*
@@ -90,5 +94,5 @@ void	hud_init(t_sdl *s, t_env *e)
 	some_traits(e);
 	actualize_background(s, e);
 	print_text(ft_strdup(s->hud1.mess[0]), s->font.color[4], s, &s->hud1.info);
-	s->hud1.info.rect = init_sdl_rect(COL4 + 28, (WIN_Y / 14) * 13.4 , 500, 25);
+	s->hud1.info.rect = init_sdl_rect(COL4 + 28, (WIN_Y / 14) * 13.4, 500, 25);
 }
