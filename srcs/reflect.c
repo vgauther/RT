824c3680eb8dd22a2b703b01666ef235673efd51
Florian DE SOUZA <fde-souz@student.42.fr>
2018-05-02 12:01:03 +0200

    norm

--- srcs/reflect.c
+++ srcs/reflect.c
@@ -6,7 +6,7 @@
 /*   By: fde-souz <fde-souz@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2018/04/24 15:21:13 by fde-souz          #+#    #+#             */
-/*   Updated: 2018/04/30 14:33:45 by fde-souz         ###   ########.fr       */
+/*   Updated: 2018/04/30 16:10:28 by fde-souz         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -55,7 +55,7 @@ Uint32		get_reflect(t_env *e, t_inter pt, int *depth, Uint32 color)
 	ref = normalize_vec(sub_vec(pt.pos, e->ca.pos));
 	ref = sub_vec(ref, v_scale(2 * dot(ref, pt.normal), &pt.normal));
 	pt.pos = add_vec(pt.pos, v_scale(0.01, &ref));
-	get_closest(e, &tmp, ref, pt.pos);
+	tmp = get_closest_test(e, ref, pt.pos, pt.nb);
 	if (tmp.dist != MAX_DIST)
 	{
 		color_2 = normalize_color(split_color(lux(e, tmp)));
